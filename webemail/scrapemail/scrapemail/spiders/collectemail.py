1a8bcf3e896ad27feede266a2014ba8ced574d1d
Nitay Pan <nitay.pan@gmail.com>
2020-05-31 03:00:41 +0530

    for email scrape code added

--- webemail/scrapemail/scrapemail/spiders/collectemail.py
+++ webemail/scrapemail/scrapemail/spiders/collectemail.py
@@ -1,13 +1,42 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
 
-with open('/app/webemail/scrapemail/websites.txt','r') as reader:
-    x = reader.read()
-    print(x)
+# with open('/app/webemail/scrapemail/websites.txt','r') as reader:
+#     x = reader.readline()
+#     print(x)
 
 class CollectemailSpider(scrapy.Spider):
     name = 'collectemail'
-    start_urls = ['http://www.rougecalgary.com/']
+
+    # with open('/app/webemail/scrapemail/websites.txt','r') as reader:
+    #     x = reader.read()
+    #     print(x)
+    web = open('websites.txt')
+    lis = list(web)
+    print(lis)
+    for i in lis:
+        start_urls = ['http://www.rougecalgary.com/',i]
 
     def parse(self, response):
-        pass
+        Url = response.request.url
+        try:
+            emails = re.findall("[A-Za-z0-9._-]+@[a-z0-9_-]+[.]+[a-z]+", str(response.body))
+            if emails != []:
+                uniquemail = set(emails)
+                mail = set()
+                for i in uniquemail:
+                    if i.endswith('png') or i.endswith('jpg') or i.endswith('gif') or i.endswith('press') or i.endswith('food') or i.endswith('time') or i.endswith('test.com') or i.endswith('mail.com') or i.endswith('example.com') or i.endswith('press.io'):
+                        continue
+                    else:
+                        mail.add(i)
+                emailid = " | ".join(mail)
+            else:
+                contact = response.xpath('//a[contains(@href,"contact")]/@href').get()
+                contactlink = response.urljoin(contact)
+                yield scrapy.Request(contactlink, callback=self.parse)
+                # emailid = 'N/A'
+        except:
+            pass
+        if emailid != '':
+            yield {'Email':emailid, "Url":Url}
